33b6e776626734c187525c2c9db03cd1d11fce64
Matia <matia.casu@gmail.com>
2021-03-04 12:23:46 +0100

    Aggiunta validazione es2

--- snack2/js/script.js
+++ snack2/js/script.js
@@ -1,7 +1,10 @@
 /*Inserisci un numero, se è pari stampa il numero, se è dispari
 stampa il numero successivo.*/
 
-var n = parseInt(prompt("inserisci numero!"));
+var n;
+do {
+  n = parseInt(prompt("Inserisci numero!"));
+} while (isNaN(n));
 
 if(n % 2 == 0){
   console.log(n + 1);
